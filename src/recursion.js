7003ea578042e1ee1005622a0d5b75dc5148cead
Yingchen <pepperbai1114@gmail.com>
2021-06-03 16:06:01 -0700

    second question

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,13 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if(array.length === 1) {
+    return array[0];
+  }
+  return array[0] + sum(array.slice(1));
 
 };
 
