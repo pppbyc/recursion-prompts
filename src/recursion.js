4e1c22a28c72316b2a6542eb223de64b0b2d60ab
Yingchen <pepperbai1114@gmail.com>
2021-06-03 17:26:45 -0700

    q4

--- src/recursion.js
+++ src/recursion.js
@@ -48,6 +48,14 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
+  if(n === 0) {
+    return true;
+  } else if (Math.abs(n) === 1) {
+    return false;
+  } else {
+    return isEven(Math.abs(n) - 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
