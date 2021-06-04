20d4b9e59507bcc928685ed8e17b53b7cd3c2565
Yingchen <pepperbai1114@gmail.com>
2021-06-03 22:34:15 -0700

    q9

--- src/recursion.js
+++ src/recursion.js
@@ -144,10 +144,15 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return "";
+  }
+    return reverse(string.substr(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
