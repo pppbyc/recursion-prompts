523c1e17d5baab0a7defc88c4dd2151e26332345
Yingchen <pepperbai1114@gmail.com>
2021-06-03 17:08:40 -0700

    third question

--- src/recursion.js
+++ src/recursion.js
@@ -34,7 +34,17 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
-};
+  var sum = 0;
+  array.forEach(function(element) {
+    if(Array.isArray(element)) {
+      sum += arraySum(element);
+    } else {
+      sum += element;
+    }
+  });
+  return sum;
+
+}
 
 // 4. Check if a number is even.
 var isEven = function(n) {
